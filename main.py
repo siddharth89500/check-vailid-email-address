b218de1ff17929becefdae60fcd25f4fa62b2d2e
siddharth89500 <sidiyadav0987@gmail.com>
2025-01-01 05:31:38 -0800

    main.py

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ email = input("enter your Email Id :")
 k,j,d=0,0,0
 if len(email)>=6:
     if email[0].isalpha():#check the start with alphabate 
-        if ("@" in email) and (email.count("@")==1): 
+        #check "@" in email only at once
+        if ("@" in email) and (email.count("@")==1):
+            #"." apears only third and fourth place 
            if (email[-4]==".") ^ (email[-3]=="."):
+               
                 for i in email:
                     if i==i.isspace():       
                       k=1
